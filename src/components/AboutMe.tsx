be0336bf3380fd178df578f3b29891a459b5e6b7
Joanna Lau <118241363+sunray4@users.noreply.github.com>
2025-10-23 13:29:20 -0400

    sun svg can move on scroll while background stays put

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,11 +1,51 @@
-import React from 'react'
+'use client';
+
+import React, { useEffect, useRef } from "react";
 import HeroTextSvg from '@/assets/hero-text.svg'
+import SunSVG from '@/assets/sun.svg'
+import gsap from 'gsap'
+import { ScrollTrigger } from "gsap/ScrollTrigger";
+import { ReactLenis } from 'lenis/react'
+
+gsap.registerPlugin(ScrollTrigger);
 
 function AboutMe() {
-  console.log(HeroTextSvg)
+  const lenisRef = useRef<any>(null)
+  // let tweebeen: gsap.core.Tween = gsap.to("#sun-svg", {})
+  
+  useEffect(() => {
+    function update(time: number) {
+      lenisRef.current?.lenis?.raf(time * 1000);
+    }
+
+    gsap.ticker.add(update);
+    gsap.registerPlugin(ScrollTrigger);
+
+    // GSAP scroll animation for the sun
+    gsap.to("#sun-svg", {
+      x: "-60vw",
+      y: "100vh",
+      ease: "none",
+      scrollTrigger: {
+        trigger: "#aboutme-section",
+        start: "top top",
+        end: "bottom bottom",
+        pinnedContainer: "#hero-text-svg",
+        scrub: true,
+      },
+    });
+
+    return () => gsap.ticker.remove(update);
+  }, []);
+
+  
   return (
-    <div className='h-[100vh]'>
-      <HeroTextSvg width="fill" height="fill" />
+    <div id="aboutme" className="relative h-[300vh]">
+      <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
+      <div className="sticky top-0 h-screen overflow-hidden">
+        <SunSVG id="sun-svg" className='absolute top-2 right-2 z-20' />
+        <HeroTextSvg id="hero-text-svg" className='absolute z-10 top-0 left-0 translate-y-1/10 max-w-full max-h-full object-contain' />
+      </div>
     </div>
   )
 }
