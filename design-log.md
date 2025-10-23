e03b6e42b6974fcdd3e1b3c87abd435bdb958300
Joanna Lau <118241363+sunray4@users.noreply.github.com>
2025-10-23 17:53:51 -0400

    change sun color and reverse travel path

--- design-log.md
+++ design-log.md
@@ -72,5 +72,6 @@ I did not realize that the several fonts that I was using in my design couldn't 
 ###### Time spent: ~45 min
 ![design image](assets/design-log-16.png)
 
-1 12
-1 15
+I realized that the current color of the sun in the hero section doesnt really look that great, so I experimented with some different colors to choose a new color.
+###### Time spent: ~7 min
+![design image](assets/design-log-17.png)
--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -1,7 +1,7 @@
 'use client';
 
 import React, { useEffect, useRef } from "react";
-import HeroTextSvg from '@/assets/hero-text.svg'
+import HeroTextSvg from '@/assets/hero-img.svg'
 // import SunSVG from '@/assets/sun.svg'
 // import SunPathSVG from '@/assets/sun-path.svg'
 import gsap from 'gsap'
@@ -9,9 +9,6 @@ import { ScrollTrigger, MotionPathPlugin } from "gsap/all";
 import { ReactLenis } from 'lenis/react'
 
 gsap.registerPlugin(ScrollTrigger, MotionPathPlugin);
-
-// NOTE: hero text is currently absolutely positioned, 
-// create a new svg that allows it to be correctly positioned with just flex justify-center items-center w-full h-full by importing with padding included
 
 function AboutMe() {
   const lenisRef = useRef<any>(null)
@@ -29,11 +26,13 @@ function AboutMe() {
         path: "#sun-path-svg",
         align: "#sun-path-svg",
         alignOrigin: [0.5, 0.5],
+        start: 1,
+        end: 0,
       },
       scale: 1.5,
       keyframes: [
-        { scale: 4, duration: 0.5 },
-        { scale: 1.2, duration: 0.5 },
+        { scale: 7, duration: 0.5 },
+        { scale: 1, duration: 0.5 },
       ],
       scrollTrigger: {
         trigger: "#aboutme",
@@ -52,13 +51,15 @@ function AboutMe() {
       <ReactLenis root options={{ autoRaf: false }} ref={lenisRef} />
       <div className="sticky top-0 h-screen overflow-hidden">
         {/* <SunSVG id="sun-svg" className='absolute z-20 top-2 right-2' /> */}
-        <svg width="1257px" height="1040px" viewBox="0 0 1257 1040" className="absolute top-12 right-12 overflow-visible mix-blend-difference" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
+        <svg width="1257px" height="1040px" viewBox="0 0 1257 1040" className="absolute top-12 right-12 overflow-visible" version="1.1" xmlns="http://www.w3.org/2000/svg" xmlnsXlink="http://www.w3.org/1999/xlink">
             <g stroke="none" strokeWidth="1" fill="none" fillRule="evenodd">
-                <image id="sun-svg" href="/sun.svg" width="100" height="100" />
+                <image id="sun-svg" href="/sun-red.svg" width="100" height="100" />
                 <path id="sun-path-svg" d="M1256,1 C937.381352,58.9333558 676.402891,172.388108 473.064619,341.364258 C269.726346,510.340407 112.226305,742.885655 0.564495153,1039" stroke="transparent"></path>
             </g>
         </svg>
-        <HeroTextSvg id="hero-text-svg" className='absolute z-10 top-0 left-2 translate-y-1/10 max-w-full max-h-full object-contain text-black mix-blend-difference' />
+        <div className="flex justify-center items-center ">
+          <HeroTextSvg id="hero-text-svg" className='max-w-full max-h-screen w-auto h-auto object-contain z-10'/>
+        </div>
       </div>
     </div>
   )
