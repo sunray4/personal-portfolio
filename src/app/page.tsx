a2197b3e14d9722952e29f080a101a5f2691c6c6
Joanna Lau <118241363+sunray4@users.noreply.github.com>
2025-10-26 14:32:33 -0400

    Revert "trying to only initialize gsap once in page but breaks some transitions"
    This reverts commit a3e67104451ef00b1d40e5319e6eabcf96765d10.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,38 +1,9 @@
-'use client';
-
 import AboutMe from '@/components/AboutMe';
-import Experience from '@/components/Experience';
-import { ReactLenis, type LenisRef } from 'lenis/react'
-import React, { useEffect, useRef } from "react";
-import gsap from 'gsap';
-import { useGSAP } from '@gsap/react';
-
-gsap.registerPlugin(useGSAP);
 
 export default function Home() {
-  const lenisRef = useRef<LenisRef>(null);
-  
-  useGSAP(() => {
-    function update(time: number) {
-      lenisRef.current?.lenis?.raf(time * 1000);
-    }
-
-    gsap.ticker.add(update);
-
-    return () => {
-      gsap.ticker.remove(update);
-    };
-  }, { scope: lenisRef });
-
   return (
-    <div>
-      <ReactLenis 
-        root 
-        options={{ autoRaf: false }} 
-        ref={lenisRef}>  
+    <>
       <AboutMe />
-      <Experience />
-      </ReactLenis>    
-    </div>
+    </>
   );
 }
